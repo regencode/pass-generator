19977aeed81a97662ca6b6f2e70678773edf4ae4
Thomas Gozalie <114067350+regencode@users.noreply.github.com>
2023-01-08 10:38:42 +0700

    Add files via upload

--- mainPageFiles/mainPage.js
+++ mainPageFiles/mainPage.js
@@ -14,3 +14,49 @@ function toPassLibrary(){
 function toPassStrChecker(){
     window.location.href="passStrengthChecker.html";
 }
+
+
+
+const observer1 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const description = entry.target.querySelector('#description-1');
+  
+      if (entry.isIntersecting == true) {
+        description.classList.add('slide-right-animation');
+        return;
+      }
+    });
+  });
+  
+  observer1.observe(document.querySelector('#detector-1'));
+
+
+
+const observer2 = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+    const description = entry.target.querySelector('#description-2');
+
+    if (entry.isIntersecting == true) {
+    description.classList.add('slide-right-animation');
+    return;
+    }
+});
+});
+
+observer2.observe(document.querySelector('#detector-2'));
+
+
+
+
+const observer3 = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+    const description = entry.target.querySelector('#description-3');
+
+    if (entry.isIntersecting == true) {
+    description.classList.add('slide-right-animation');
+    return;
+    }
+});
+});
+
+observer3.observe(document.querySelector('#detector-3'));
