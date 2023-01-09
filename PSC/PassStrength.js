6f3fe2e96b0b07b58ce422250da5d7c7194755a9
Thomas Gozalie <114067350+regencode@users.noreply.github.com>
2023-01-09 22:38:13 +0700

    Add files via upload

--- PSC/PassStrength.js
+++ PSC/PassStrength.js
@@ -20,67 +20,14 @@ function see()
     
 }
 
-function check()
-{
-    var input = document.getElementById("password").value;
-    
-    input=input.trim();
-    document.getElementById("password").value=input;
-    document.getElementById("count").innerText="Length : " + input.length;
-    if(input.length>=5)
-    {
-        document.getElementById("check0").style.color="green";
-    }
-    else
-    {
-       document.getElementById("check0").style.color="red"; 
-    }
-    
-    if(input.length<=10)
-    {
-        document.getElementById("check1").style.color="green";
-    }
-    else
-    {
-       document.getElementById("check1").style.color="red"; 
-    }
-    
-    if(input.match(/[0-9]/i))
-    {
-        document.getElementById("check2").style.color="green";
-    }
-    else
-    {
-       document.getElementById("check2").style.color="red"; 
-    }
-    
-    if(input.match(/[^A-Za-z0-9-' ']/i))
-    {
-        document.getElementById("check3").style.color="green";
-    }
-    else
-    {
-       document.getElementById("check3").style.color="red"; 
-    }
-    
-    if(input.match(' '))
-    {
-        document.getElementById("check4").style.color="red";
-    }
-    else
-    {
-       document.getElementById("check4").style.color="green"; 
-    }
-    
-}
 
 
 function getPasswordStrength(password){
     let s = 0;
-    if(password.length > 6){
+    if(password.length > 8){
       s++;
     }
-    if(password.length > 10){
+    if(password.length > 12){
       s++;
     }
     if(/[A-Z]/.test(password)){
@@ -94,6 +41,8 @@ function getPasswordStrength(password){
     }
     return s;
   }
+
+
   document.querySelector(".pw-meter #password").addEventListener("focus",function(){
     document.querySelector(".pw-meter .pw-strength").style.display = "block";
   });
@@ -127,5 +76,43 @@ function getPasswordStrength(password){
       passwordStrengthSpans[0].style.color = "#fff";
       passwordStrengthSpans[1].style.background = "#20a820";
     }
-  });
 
+    
+    if(password.length>=8)
+    {
+        document.getElementById("check0").style.color = "green";
+    }
+    else
+    {
+      document.getElementById("check0").style.color = "red";
+    }
+
+    if(password.match(/[A-Z]*/i))
+    {
+      document.getElementById("check1").style.color = "green";
+    }
+    else
+    {
+      document.getElementById("check1").style.color = "red";
+    }
+
+
+    if(password.match(/[0-9]/i))
+    {
+      document.getElementById("check2").style.color = "green";
+    }
+    else
+    {
+      document.getElementById("check2").style.color = "red";
+    }
+    
+    if(password.match(/[^A-Za-z0-9-' ']/i))
+    {
+      document.getElementById("check3").style.color = "green";
+    }
+    else
+    {
+      document.getElementById("check3").style.color = "red";
+    }
+    
+  });
